2c7b33740bb78268e85449cac0aab30ce957afa3
Ferreira-well <w.ferreira.lopes04@gmail.com>
2023-11-29 20:57:21 -0300

    Add files via upload

---
+++ Java-Exercicios/05.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Iteração externa: " + i);
+
+            for (int j = 1; j <= 5; j++) {
+               
+                if (j == 3) {
+                    continue;
+                }
+
+                System.out.println("   Iteração interna: " + j);
+            }
+        }
+    }
+}
